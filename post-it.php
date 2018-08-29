988637965449a22d703e2667cff4afdf0c19bd48
Seeger01 <cph-ps183@cphbusiness.dk>
2018-08-30 00:16:16 +0200

    post-it first try
    have made a tags and a hover effect.

---
+++ post-it.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Post-it</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+</head>
+
+<body>
+	<div class="postit">
+		<a href="#">
+		<time>Dato</time>
+		<h2>Overskrift</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, eveniet distinctio nesciunt quis.</p>
+		<p class="name">MORO</p>
+		</a>
+	</div>
+		<div class="postit">
+		<a href="#">
+		<time>Dato</time>
+		<h2>Overskrift</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, eveniet distinctio nesciunt quis.</p>
+		<p class="name">MORO</p>
+		</a>
+	</div>
+		<div class="postit">
+		<a href="#">
+		<time>Dato</time>
+		<h2>Overskrift</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, eveniet distinctio nesciunt quis.</p>
+		<p class="name">MORO</p>
+		</a>
+	</div>
+		<div class="postit">
+		<a href="#">
+		<time>Dato</time>
+		<h2>Overskrift</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, eveniet distinctio nesciunt quis.</p>
+		<p class="name">MORO</p>
+		</a>
+	</div>
+		<div class="postit">
+		<a href="#">
+		<time>Dato</time>
+		<h2>Overskrift</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, eveniet distinctio nesciunt quis.</p>
+		<p class="name">MORO</p>
+		</a>
+	</div>
+		<div class="postit">
+		<a href="#">
+		<time>Dato</time>
+		<h2>Overskrift</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, eveniet distinctio nesciunt quis.</p>
+		<p class="name">MORO</p>
+		</a>
+	</div>
+</body>
+</html>
